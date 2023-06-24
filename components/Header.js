11dfeba1a9346f9246e29c81d4a8df5737b8a6b9
Akshay <20bcs59@nith.ac.in>
2023-06-24 20:49:29 +0530

    login

--- components/Header.js
+++ components/Header.js
@@ -2,12 +2,19 @@ import React from 'react'
 import Navbar from './Navbar'
 import styles from '../styles/header.module.css'
 import { useSession } from 'next-auth/react'
+import Avatar from '@mui/material/Avatar';
+import Chip from '@mui/material/Chip';
 
 function Header() {
-  const {data:session,status}=useSession()
-  const Login_=()=>{
-    if(status==='authenticated'){
-    return <h1 >{session.user.name} </h1>
+  const { data: session, status } = useSession()
+  const Login_ = () => {
+    if (status === 'authenticated') {
+      return <Chip
+              avatar={<Avatar alt="Natacha" src={session.user.image} />}
+              label={session.user.name}
+              variant="outlined"
+              id='avatar'
+             />
     }
   }
 
@@ -16,14 +23,14 @@ function Header() {
 
       <div className={styles.header}>
         <div className={styles.overlay}></div>
-        
-        <video src="/bg1.mp4" autoPlay loop muted className={styles.bgVideo}/>
-        <Navbar/>
+
+        <video src="/bg1.mp4" autoPlay loop muted className={styles.bgVideo} />
+        <Navbar />
         <div className={styles.content}>
-            
-        <Login_></Login_>
-        <h1>This is NITH </h1>
-            <p>A leading centre of computer science research and education in India.</p>
+
+          <Login_></Login_>
+          <h1>This is NITH </h1>
+          <p>A leading centre of computer science research and education in India.</p>
         </div>
       </div>
     </>
