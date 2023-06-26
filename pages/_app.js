5bbe6349405f34bfb3be3d30950283b39d4a931b
Akshay <20bcs59@nith.ac.in>
2023-06-26 22:25:07 +0530

    final

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import { SessionProvider } from "next-auth/react"
 
 
 export default function App({session, Component, pageProps }) {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setInterval(() => {
