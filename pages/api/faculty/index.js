6a1e33e1cd4815b47936b2b293873ca71872db88
lekhit borole <blekhit@gmail.com>
2023-06-27 17:36:35 +0530

    updated faculty

---
+++ pages/api/faculty/index.js
@@ -0,0 +1,28 @@
+
+
+import deta from '../../middleware/deta'
+
+
+
+export default async function handler(
+  req,
+  res
+) {
+
+let new_users;
+
+
+  try{
+  //   new_user = await prisma.user.findFirst({
+  //  where:{
+  //   email:email
+  //  },
+  // })
+new_users=await deta.Base('faculty').fetch({})
+
+}catch(err){console.log(err)} 
+  // If you don't have the NEXTAUTH_SECRET environment variable set,
+  // you will have to pass your secret as `secret` to `getToken`
+
+res.send(JSON.stringify(new_users))
+}
