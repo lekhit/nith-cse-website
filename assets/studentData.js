e06972eb499d2a7a58046578cf32f914a2780de8
Akshay garg <gargakshay034@gmail.com>
2023-06-27 12:55:23 +0530

    Update studentData.js

--- assets/studentData.js
+++ assets/studentData.js
@@ -12,7 +12,7 @@ export const studentData = [
         about: "Ph.D, Marketing Strategy & International Business, The University of Texas at Austin , 1992 -1996 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto ipsa earum aspernatur numquam ex alias hic magni exercitationem, unde sit enim facere distinctio eius aperiam molestiae autem eos a ab qui voluptatem!"
     },
     {
-        id:1,
+        id:2,
         name: "Shyam",
         rollno: "20BCS070",
         skills: ["HTML", "ReactJS", "NextJS"],
@@ -24,7 +24,7 @@ export const studentData = [
         about: "Ph.D, Marketing Strategy & International Business, The University of Texas at Austin , 1992 -1996 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto ipsa earum aspernatur numquam ex alias hic magni exercitationem, unde sit enim facere distinctio eius aperiam molestiae autem eos a ab qui voluptatem!"
     },
     {
-        id:1,
+        id:3,
         name: "Arjun",
         rollno: "20BCS085",
         skills: ["HTML", "ReactJS", "NextJS"],
@@ -36,7 +36,7 @@ export const studentData = [
         about: "Ph.D, Marketing Strategy & International Business, The University of Texas at Austin , 1992 -1996 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto ipsa earum aspernatur numquam ex alias hic magni exercitationem, unde sit enim facere distinctio eius aperiam molestiae autem eos a ab qui voluptatem!"
     },
     {
-        id:1,
+        id:4,
         name: "Ashutosh",
         rollno: "20BCS069",
         skills: ["HTML", "ReactJS", "NextJS"],
@@ -48,7 +48,7 @@ export const studentData = [
         about: "Ph.D, Marketing Strategy & International Business, The University of Texas at Austin , 1992 -1996 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto ipsa earum aspernatur numquam ex alias hic magni exercitationem, unde sit enim facere distinctio eius aperiam molestiae autem eos a ab qui voluptatem!"
     },
     {
-        id:1,
+        id:5,
         name: "Lekhit",
         rollno: "20BCS064",
         skills: ["HTML", "ReactJS", "NextJS"],
